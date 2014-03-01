ad47c3ada6bd5d28dde3d3006359987071b1b429
Yousef Alam <flamingspaz96@googlemail.com>
2014-03-01 16:31:44 +0000

    test

--- manifests/discourse-update.pp
+++ manifests/discourse-update.pp
@@ -1,9 +1,9 @@
-exec { "update_discourse":
+exec { "stop_bluepill":
 	user	=> "discourse",
 	path    => "/var/www/discourse",
     command => "bluepill stop",
 }
-exec { "update_discourse":
+exec { "quit_bluepill":
 	user	=> "discourse",
 	path    => "/var/www/discourse",
     command => "bluepill quit",
