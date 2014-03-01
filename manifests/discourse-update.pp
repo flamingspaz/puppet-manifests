7f89dd525a9e0ecc013c7de5dff2d5c344c4e56a
Yousef Alam <flamingspaz96@googlemail.com>
2014-03-01 16:41:52 +0000

    test

--- manifests/discourse-update.pp
+++ manifests/discourse-update.pp
@@ -2,7 +2,8 @@ exec { "stop_bluepill":
 	user	=> "discourse",
 	path	=> "/home/discourse/.rvm/gems/ruby-2.0.0-p353/bin/",
 	cwd		=> "/var/www/discourse",
-    command => "bluepill stop",
+	environment => "NOEXEC_DISABLE=1"
+    command => "bluepill --no-privileged -c ~/.bluepill stop",
 }
 exec { "quit_bluepill":
 	user	=> "discourse",
